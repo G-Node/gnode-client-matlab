254db72d37213c93686187cc7eeebb7e7cb62abe
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-25 21:52:18 +0200

    Implement filter function

---
+++ +gnode/filter.m
@@ -0,0 +1,24 @@
+function new = filter(session, obj, object_type)
+
+  % Returns a clean version of a given NEObject based on
+  % contracts specified in session.validator.
+
+  if (nargin < 3)
+    error('[GNODE] Object type necessary. Please specify');
+  end
+
+  legal_fields = cell(session.connector.validator.getAll(object_type));
+  legal_fields{end+1} = 'neo_id'; % Additionally, we keep neo_id
+  
+  fields = fieldnames(obj);
+  new_object = struct();
+
+  for i = 1:size(fields, 1)
+    if (any(ismember(legal_fields, fields{i})))
+      new_object = setfield(new_object, fields{i}, getfield(obj, fields{i}));
+    end
+  end
+
+  new = new_object;
+
+end
