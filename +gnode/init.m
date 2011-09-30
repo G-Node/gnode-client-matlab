75263a33dfb8efc2a49e30b8a31a4f8c9aa43829
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-30 16:10:11 +0200

    Clean init() plus MIT license

--- +gnode/init.m
+++ +gnode/init.m
@@ -1,4 +1,36 @@
+% Copyright (C) 2011 by German Neuroinformatics Node (www.g-node.org)
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
+
 function session = init(filename, password, server)
+  %INIT Creates a G-Node data store session, returning a *structure*
+  %that is passed to most G-Node utility methods.
+  %
+  %  g = init('DEFAULT') returns a session g defaulting to standard
+  %  settings. This configuration can be found (and, if necessary,
+  %  modified) under $TBDIR/default.json.
+  %
+  %  g = init(username, password, server) returns a session g using
+  %  specified authentication/server settings but defaulting to
+  %  $TBDIR/default.json for everything else.
+  %
+  %  g = 
 
   import gnode.*;
 
