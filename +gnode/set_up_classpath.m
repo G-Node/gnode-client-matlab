2cda8fbf13364bd08ebb4c7edbf90b834beb4929
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-30 15:04:39 +0200

    Clean and comment set_up_classpath()

--- +gnode/set_up_classpath.m
+++ +gnode/set_up_classpath.m
@@ -1,7 +1,22 @@
-function nothing = set_up_classpath()
+%GNODE.SET_UP_CLASSPATH Initialises MATLAB JVM.
+% 
+%  No parameters. Uses toolboxdir() in order to determine location of the
+%  library JAR. Additionally, various JVM properties are set to sane
+%  standard values.
 
-  javaaddpath('/home/aleonhardt/matlab-client/gnode-client-lib/target/gnode-client-lib-assembly-0.2.jar');
+function set_up_classpath()
+
+  % Settings
+  toolbox_name = 'gnode';
+  lib_file = 'client.jar';
+  
+  lib_path = fullfile(toolboxdir(toolbox_name), 'lib', lib_file);
+
+  % Set classloader path to include library JAR
+  javaaddpath(lib_path);
   clear java;
+
+  % Fix inconsistency (till R2008b) involving illegal default proxy settings
   java.lang.System.clearProperty('https.proxyPort');
   java.lang.System.clearProperty('http.proxyPort');
 
