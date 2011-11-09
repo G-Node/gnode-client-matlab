092fd5779ebcca553419e9a10a6cdfda45ed5068
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-11-09 23:47:30 +0100

    Improve documentation for get_updated()

--- +gnode/get_updated.m
+++ +gnode/get_updated.m
@@ -1,8 +1,13 @@
 function u = get_updated(session, obj, id)
+  %GET_UPDATED Retrieves object after performing a remote update;
+  %documentation for wrapped get()/update() functions applies.
+  %
+  %  updated_object = get_updated(g, modified_object, 'analogsignal_947') pushes changes to remote server and returns the stored object
+  %  including successfully applied changes.
 
   import gnode.*;
 	 
-  % Update object and return updated copy!
+  % Update object and return updated copy
 
   if (nargin < 3)
     updated_id = update(session, obj){1};
@@ -13,3 +18,23 @@ function u = get_updated(session, obj, id)
   u = get(session, updated_id);
 
 end
+
+% Copyright (C) 2011 by German Neuroinformatics Node (www.g-node.org)
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
