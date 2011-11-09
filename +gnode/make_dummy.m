23a33a973caaf77284b5d471e3c2620d7d51cc75
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-11-10 00:23:58 +0100

    Add documentation

--- +gnode/make_dummy.m
+++ +gnode/make_dummy.m
@@ -1,9 +1,14 @@
 function dummy = make_dummy(session, object_type, minimal)
+  %MAKE_DUMMY Returns a "readymade" structure for creating new
+  %objects with all available (valid) fields added but left blank.
+  %Key purpose is rapid creation of G-Node data store objects by
+  %MATLAB users.
+  %
+  %  dummy = make_dummy(g, 'analogsignal') returns a structure
+  %  containing empty fields for all signal-related properties
+  %  that are required by the G-Node validator.
 
   import gnode.*;
-
-  % Returns a "readymade" structure for creating new objects
-  % with all available (valid) fields added but left blank.
 
   if (nargin < 3)
     minimal = false;
@@ -41,3 +46,23 @@ function dummy = make_dummy(session, object_type, minimal)
   dummy = prep_dummy;
 
 end
+
+% Copyright (C) 2011 by German Neuroinformatics Node (www.g-node.org)
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
