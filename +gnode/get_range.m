b19cb12fadcae3ad8418820b1b47a13acd2d97a7
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-11-09 23:34:58 +0100

    Improve performance of range retrieval

--- +gnode/get_range.m
+++ +gnode/get_range.m
@@ -1,4 +1,10 @@
 function l = get_range(session, object_type, start, stop)
+  %GET_RANGE Retrieves multiple G-Node data store objects of given
+  %type between two numerical markers.
+  %
+  %  objects = get_range(g, 'analogsignal', 100, 500) returns all
+  %  available signals between IDs 'analogsignal_100' and
+  %  'analogsignal_500'.
 
   n = stop - start + 1;
   ids = cell(1,n);
@@ -10,3 +16,23 @@ function l = get_range(session, object_type, start, stop)
   l = gnode.get(session, ids.');
 
 end
+
+% Copyright (C) 2011 by German Neuroinformatics Node (www.g-node.org)
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
