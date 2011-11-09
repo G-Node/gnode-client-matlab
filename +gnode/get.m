ca0256a16321a2f510138a35563dbec2ecd3caa9
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-11-09 22:56:30 +0100

    Add documentation/use cases to get()

--- +gnode/get.m
+++ +gnode/get.m
@@ -1,17 +1,36 @@
-function s = get(session, request)
-
+function result = get(session, request)
+  %GET Retrieves a single or multiple G-Node data store objects from
+  %the remote container. If possible (i.e., for objects not recently
+  %modified), a cached representation is returned. For several
+  %objects, use cell arrays of IDs. Return order is not guaranteed.
+  %
+  %  remote_object = get(g, 'analogsignal_947') returns a structure
+  %  corresponding to form and content of the indicated data object.
+  %
+  %  remote_objects = get(g, {'analogsignal_47', 'block_5'}) returns
+  %  an array of objects corresponding to form and content of the
+  %  indicated data objects.
+  
+  % Pull auxiliary functions into scope
   import gnode.*;
 
+  % Sanity check
+  if (nargin < 2)
+    error('[GNODE] Insufficient parameters');
+  end
+
+  % Container for return objects
   my_objects = {};
   
-  if (~iscellstr(request))
+  if (~iscellstr(request)) % Single request
      
     obj = session.connector.retrieve(request);
-    if (~is_none(obj))
+    
+    if (~is_none(obj)) % Scala-specific check of Some-typed result
       my_objects = serialize(obj.get);
     end
 
-  else
+  else % Multiple requests
 
     for k = 1:size(request,1)
       obj = session.connector.retrieve(request{k});
@@ -22,6 +41,26 @@ function s = get(session, request)
 
   end
 
-  s = my_objects;
+  result = my_objects;
 
 end
+
+% Copyright (C) 2011 by German Neuroinformatics Node (www.g-node.org)
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
