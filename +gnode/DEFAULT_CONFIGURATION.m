d36aaf989ce1bc7506e49ad8e8a00b8d7885564a
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-11-09 22:25:19 +0100

    Add doc string to DEFAULT_CONFIGURATION()

--- +gnode/DEFAULT_CONFIGURATION.m
+++ +gnode/DEFAULT_CONFIGURATION.m
@@ -1,3 +1,31 @@
 function value = DEFAULT_CONFIGURATION
+  %DEFAULT_CONFIGURATION Returns the keyword associated with default
+  %configuration of toolbox. To be called in conjunction with init()
+  %
+  %  g = init(DEFAULT_CONFIGURATION) returns a session g defaulting
+  %  to standard settings. This configuration can be found (and, if
+  %  necessary, modified) under $TBDIR/default.json.
+  
   value = 'default';
+  
 end
+
+% Copyright (C) 2011 by German Neuroinformatics Node (www.g-node.org)
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
