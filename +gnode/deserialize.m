d99fa476ebb837f9c7d52530ce59991ed50063a3
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-11-10 00:26:21 +0100

    Fix deserialize()

--- +gnode/deserialize.m
+++ +gnode/deserialize.m
@@ -1,4 +1,10 @@
 function neo_obj = deserialize(s)
+  %DESERIALIZE Internal function that builds NEObject from appropriate
+  %structure. Does not perform validation; only safe for pre-
+  %validated MATLAB structs.
+  %
+  %  neo_object = deserialize(str) returns a Java object corresponding
+  %  to contents of passed structure.
 
   import org.gnode.lib.neo.*;
   b = NEOBuilder;
@@ -35,3 +41,23 @@ function neo_obj = deserialize(s)
   neo_obj = b.build;
 
 end
+
+% Copyright (C) 2011 by German Neuroinformatics Node (www.g-node.org)
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
