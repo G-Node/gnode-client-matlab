2bb01931c1fc07fa408c87824ca24944fe5fb2d4
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-11-09 23:50:07 +0100

    Document is_none()

--- +gnode/is_none.m
+++ +gnode/is_none.m
@@ -1,4 +1,31 @@
 function is_it = is_none(obj)
+  %IS_NONE Internal convenience function. Wraps isNone() provided
+  %by G-Node client library. Checks if the supplied object of type
+  %Some[T] is Scala's None.
+  %
+  %  is_none(obj) evaluates to True if and only if 'obj' is None.
+  
   import org.gnode.lib.matlab.Helper;
   is_it = Helper.isNone(obj);
+  
 end
+
+% Copyright (C) 2011 by German Neuroinformatics Node (www.g-node.org)
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
