23b344229613adc0cbb2e978118057f076b3e829
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-11-09 23:32:01 +0100

    Fix limit in get_list()

--- +gnode/get_list.m
+++ +gnode/get_list.m
@@ -1,4 +1,10 @@
 function l = get_list(session, object_type, limit)
+  %GET_LIST Retrieves an exhaustive list of all objects of specified
+  %object type that are available in the specified session. A limit
+  %may be specified.
+  %
+  %  list = get_list(g, 'analogsignal', 100) returns a list of all
+  %  available signals limited to 100 entries.
 
   if (nargin < 3)
     limit = 0;
@@ -11,3 +17,23 @@ function l = get_list(session, object_type, limit)
   end
 
 end
+
+% Copyright (C) 2011 by German Neuroinformatics Node (www.g-node.org)
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
