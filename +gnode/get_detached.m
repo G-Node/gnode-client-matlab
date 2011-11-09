e9a3eeddb55a9584f56576fcacf346d00d93d534
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-11-09 23:24:57 +0100

    Fix minor issues and documentation in get_detached()

--- +gnode/get_detached.m
+++ +gnode/get_detached.m
@@ -1,9 +1,24 @@
 function detached = get_detached(session, request, named)
+  %GET_DETACHED Convenience function that returns raw signal data
+  %only. Works with analogsignal, irssanalogsignal, spiketrain.
+  %Final parameter, 'named', determines whether the data arrays
+  %are to be returned as either
+  %
+  %a) a structure with field names corresponding to object names, or
+  %b) a cell array containing the data.
+  %
+  %  raw = get_detached(g, 'analogsignal_947') returns only signal
+  %  data associated with remote object without meta-information.
+  %
+  %  raw = get_detached(g, obj_names, true) returns a struct whose
+  %  fields correspond to signal data of specified remote objects.
+  %  Field names are equivalent to object names.
 
   import gnode.*;
 
-  % Convenience function that returns raw signal data only. Works
-  % with analogsignal, irssanalogsignal, spiketrain.
+  if (nargin < 2)
+    error('[GNODE] Insufficient arguments');
+  end
 
   if (nargin < 3)
     named = false;
@@ -45,3 +60,23 @@ function detached = get_detached(session, request, named)
   end
 
 end
+
+% Copyright (C) 2011 by German Neuroinformatics Node (www.g-node.org)
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
