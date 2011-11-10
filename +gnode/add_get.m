6c3ab8bac26933c12de0629f538af9ae50e1d875
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-11-10 01:24:26 +0100

    Document queue functions

--- +gnode/add_get.m
+++ +gnode/add_get.m
@@ -1,4 +1,11 @@
 function add_get(session, request)
+  %ADD_GET Adds a retrieval job to the task queue. By calling get_all(),
+  %all tasks are performed in one go.
+  %
+  %  add_get(g, 'analogsignal_947');
+  %  add_get(g, 'analogsignal_948');
+  %
+  %  obj = get_all(g) now returns all queued signals.
 
   import gnode.*;
 
@@ -11,3 +18,23 @@ function add_get(session, request)
   end
 
 end
+
+% Copyright (C) 2011 by German Neuroinformatics Node (www.g-node.org)
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
--- +gnode/get_all.m
+++ +gnode/get_all.m
@@ -1,7 +1,17 @@
 function s = get_all(session)
+  %GET_ALL Retrieves all objects placed in the task queue via add_get(). Order
+  %is not guaranteed.
+  %
+  %  add_get(g, 'analogsignal_947');
+  %  add_get(g, 'analogsignal_948');
+  %
+  %  obj = get_all(g) now returns all queued signals.
 
   import gnode.*;
+
+  % Empty container for returned objects
   my_objects = {};
+
   objects = session.connector.retrieve;
 
   for i = 1:size(objects,1)
@@ -11,3 +21,23 @@ function s = get_all(session)
   s = my_objects;
 
 end
+
+% Copyright (C) 2011 by German Neuroinformatics Node (www.g-node.org)
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
