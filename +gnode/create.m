bfa7f5ee01ac58c84a7e409fd6bc1d23f6e51a21
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-11-10 01:31:05 +0100

    Change formatting in doc string

--- +gnode/create.m
+++ +gnode/create.m
@@ -1,4 +1,13 @@
 function id = create(session, obj, object_type)
+  %CREATE Creates new object of specified object type on remote G-Node
+  %data store. Attempts to guess object type where possible; explicit
+  %argument is, however, preferred. New object needs to pass both
+  %client- and server-side validation. Returns generated ID for new
+  %object upon success.
+  %
+  %  new_id = create(g, new_obj, 'analogsignal') returns ID of newly
+  %  stored signal, and throws an exception in case of transmission
+  %  problems.
 
   import gnode.*;
 	 
@@ -32,3 +41,23 @@ function id = create(session, obj, object_type)
   id = cell(ret);
 
 end
+
+% Copyright (C) 2011 by German Neuroinformatics Node (www.g-node.org)
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
