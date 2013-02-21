86e786169e2f2c30efd10e564be682d9516d3137
Aljoscha Leonhardt <apleonhardt@gmail.com>
2013-02-21 14:14:51 +0100

    Fix sharing function

--- +gnode/share.m
+++ +gnode/share.m
@@ -1,23 +1,30 @@
-function share(session, objects, user, level, cascade)
+function share(session, objects, safety_level, users, levels, cascade)
 %SHARE Modify an entity's permissions. The required parameters are session,
 %either a single object (or its ID) or a list of objects (or their IDs),
-%the user in question, safety level (1 = public, 2 = group, 3 = private),
-%and whether the modification should apply to all nested objects (cascade).
+%safety level (1 = public, 2 = group, 3 = private), the user(s) in question
+%(as a cell array), user access levels (as an array), and whether the
+%modification should apply to all nested objects (cascade).
 %
-%  gnode.share(session, 'analogsignal_1', 'john', '1', 0);
+%  gnode.share(session, 'analogsignal_1', 1, {'john'}, [2], 0) makes
+%  'analogsignal_1' publicly available and writable for user john. In this
+%  case, changes do not cascade to nested objects.
 
 %% Defaults
 
-if nargin < 5
+if nargin < 6
     cascade = 0;
 end
 
+if nargin < 5
+    levels = [0];
+end
+
 if nargin < 4
-    level = 1;
+    users = {''};
 end
 
 if nargin < 3
-    error('[GNODE] Please specify user');
+    error('[GNODE] Please specify safety level!');
 end
 
 %% Argument classification
@@ -53,10 +60,15 @@ if ~iscellstr(objects)
     error(err_msg);
 end
 
+% Convert cell array to Java string array
+j_users = javaArray('java.lang.String', length(users));
+for idx = 1:length(users), j_users(idx) = java.lang.String(users{idx}); end
+
 %% Share
 
 for o = 1:length(objects)
-    if ~session.connector.shareObject(objects{o}, user, level, cascade);
+    if ~session.connector.shareObject(objects{o}, safety_level, users, ...
+            levels, cascade);
         error('[GNODE] Could not share object %s', obj);
     end
 end
